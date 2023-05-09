65938d2e5f99863a9afc1f4fe5972b2b56fb6856
Bishal <98676bishal@gmail.com>
2023-05-09 20:44:11 +0545

    base url changed to domain

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -4,7 +4,8 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
       baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        // "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+        "http://www.caffeinecentral.online",
       headers: req.headers,
       timeout: 10000,
     });
